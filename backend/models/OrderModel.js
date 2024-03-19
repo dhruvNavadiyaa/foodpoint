a12bc9c01252a7b92df7d1e5b71b14521b9e121f
talaviyayash <95530124+talaviyayash@users.noreply.github.com>
2024-03-19 14:35:23 +0530

    ContactUs Model And Controller

--- backend/models/OrderModel.js
+++ backend/models/OrderModel.js
@@ -37,7 +37,7 @@ const OrderSchema = new Schema({
         ref:'DeliverBoy'
     },
     address: 
-        {    area: {
+        {    fulladdress: {
                 type: String,
             },
     },
