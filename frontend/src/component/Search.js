f4256964408544cd192dd69a5e30942607fb1155
talaviyayash <95530124+talaviyayash@users.noreply.github.com>
2024-03-19 15:20:20 +0530

    Bug Fixing

--- frontend/src/component/Search.js
+++ frontend/src/component/Search.js
@@ -81,7 +81,7 @@ export default function Search() {
                                             <small className='fw-medium text-secondary'>{item.description}.</small>
                                         </div>
                                         <div className="col-4 ">
-                                            <img src={item?.img[0]||"https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww"}
+                                            <img src={item?.img||"https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww"}
                                                 className='img-fluid rounded rounded box-shadow' alt="" />
                                             <button className='btn btn-outline-success btn-sm'>ADD</button>
                                         </div>
