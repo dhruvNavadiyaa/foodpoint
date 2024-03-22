7bf0a5f4c7d753cb79f9f607c10b591d12c0107d
dhruvvN <dhruvna9@gmail.com>
2024-03-22 18:41:08 +0530

    done delivery partner in admin panel

--- admin/src/component/DeliveryPartner.js
+++ admin/src/component/DeliveryPartner.js
@@ -8,7 +8,7 @@ import { useSelector } from 'react-redux'
 
 export default function DeliveryPartner() {
 
-    const adminName = useSelector(state => state.admin.adminInfo.name)
+    //SIDEBAR AND MAINCOMPONENT MOVEMENT FUNCTIONS SCREEN WISE
     const hide = () => {
         if (window.innerWidth <= 750) {
             let sidebar, slide, main, title
@@ -43,51 +43,41 @@ export default function DeliveryPartner() {
         title.classList.toggle("headerMove")
     }
 
-    const delivers = {
-        "approved": [{ name: "yash", no: 8140974832, email:"xyz@gmail.com" }],
-        "pending": [{ name: "jaydeep", no: 8140974832, email:"xyz@gmail.com" }, { name: "deep", no: 8122281222, email:"xyz@gmail.com" }],
-        "rejected": [{ name: "jaydeep", no: 8140974832, email:"xyz@gmail.com" }, { name: "deep", no: 8122281222, email:"xyz@gmail.com" }, { name: "dhruv asodariya", no: 8122281222, email:"asodariya@gmail.com" }]
-    }
-
+    const [buttonState,setbButtonstate] = useState(true)
+    const adminName = useSelector(state => state.admin.adminInfo.name)
     const [deliveryBoys, setDeliveryBoys] = useState({})
-    const [currentData, setCurrentData] = useState(delivers.approved)
+    const [currentData, setCurrentData] = useState([])
+
 
     const allDeliveryBoy = async () => {
         const response = await axios.get('http://localhost:5000/api/delivery/allfetch')
         setDeliveryBoys(response.data)
+        setCurrentData(response.data.approved)
         console.log(response.data)
     }
-    
+    const approveDeliveryBoy = async (id) => {
+        const data ={
+            DeliveryBoy_id : id,
+            status : "approved"
+        }
+        const response = await axios.post('http://localhost:5000/api/delivery/approvel',data)
+        // setDeliveryBoys(response.data)
+        console.log(response.data)
+    }
+    const deleteDeliveryBoy = async (id) => {
+        const data ={
+            DeliveryBoy_id : id
+        }
+        const response = await axios.post('http://localhost:5000/api/delivery/delete',data)
+        // setDeliveryBoys(response.data)
+        console.log(response.data)
+    }
+
     useEffect(() => {
         allDeliveryBoy()
-        setCurrentData(delivers.approved)
+        // setCurrentData(deliveryBoys.pending)
         // console.log(currentData)
     }, [])
-
-    const [state,setState] = useState(false)
-    const changeData = (change) => {
-        
-        if (change === 'pending') {
-            // setCurrentData(deliveryBoys.pending)
-            setState(true);
-            setCurrentData(delivers.pending)
-        }
-        else if(change === 'rejected') {
-            // setCurrentData(deliveryBoys.rejected)
-            setState(true)
-            setCurrentData(delivers.rejected)
-        }
-        else{
-            // setCurrentData(deliveryBoys.rejected)
-            setState(false)
-            setCurrentData(delivers.approved)
-        }
-    };
-
-    const approveBoy = (no) => {
-        console.log(no)
-        
-    }
 
     return (
         <>
@@ -111,47 +101,39 @@ export default function DeliveryPartner() {
                                 <h2 className='pageName py-3 ps-3 d-flex'>
                                     Delivery Partner
                                     <div className='ms-auto me-4'>
-                                        <button className='buttons mx-2' onClick={() => { changeData('approved') }}>Approved &nbsp;
-                                            {/* <span className='bg-dark text-light rounded-5 px-2 fs-6'>{deliveryBoys.approved ? deliveryBoys.approved.length : '0'}</span> */}
-                                            <span className='bg-dark text-light rounded-5 px-2 fs-6'>{delivers.approved ? delivers.approved.length : '0'}</span>
-                                        </button>
-                                        <button className='buttons mx-2' onClick={() => { changeData('pending') }}>Pending &nbsp;
-                                            {/* <span className='bg-dark text-light rounded-5 px-2 fs-6'>{deliveryBoys.pending ? deliveryBoys.pending.length : '0'}</span> */}
-                                            <span className='bg-dark text-light rounded-5 px-2 fs-6'>{delivers.pending ? delivers.pending.length : '0'}</span>
-                                        </button>
-                                        <button className='buttons mx-2' onClick={() => { changeData('rejected') }}>Rejected &nbsp;
-                                            {/* <span className='bg-dark text-light rounded-5 px-2 fs-6'>{deliveryBoys.rejected ? deliveryBoys.rejected.length : '0'}</span> */}
-                                            <span className='bg-dark text-light rounded-5 px-2 fs-6'>{delivers.rejected ? delivers.rejected.length : '0'}</span>
-                                        </button>
+                                        <button className={`mx-2 px-3 btn btn-sm ${buttonState?'btn-outline-dark':'btn-dark'}`} onClick={()=>{setCurrentData(deliveryBoys.pending);setbButtonstate(false)}}>Pending</button>
+                                        <button className={`mx-2 px-3 btn btn-sm ${!buttonState?'btn-outline-dark':'btn-dark'}`} onClick={()=>{setCurrentData(deliveryBoys.approved);setbButtonstate(true)}}>Approved</button>
                                     </div>
                                 </h2>
 
                                 <div className="orders mt-4 p-3" >
-                                    {currentData.map((item,index) => {
-                                        return (
-                                            <div className='card-delivery text-start mb-5 box-shadow' key={index}>
-                                                <div className='d-flex'>
-                                                    <img src="https://www.svgrepo.com/show/384676/account-avatar-profile-user-6.svg" className='img-logo-delivery ms-1' alt="" />
-                                                    <small className='ms-auto mt-3 me-4'></small>
+
+                                    {
+                                        currentData.map((item, index) => {
+                                            return (
+                                                <div className='card-delivery text-start mb-5 box-shadow' key={index}>
+                                                    <div className='d-flex'>
+                                                        <img src="https://www.svgrepo.com/show/384676/account-avatar-profile-user-6.svg" className='img-logo-delivery ms-1' alt="" />
+                                                        <small className='ms-auto mt-3 me-4'></small>
+                                                    </div>
+                                                    <p className='ms-3 fw-bold'>{item.name}</p>
+                                                    <div className='d-flex px-3 mt-3' style={{ fontSize: '13px' }}>
+                                                        <p className='d-inline fw-semibold'>Email</p>
+                                                        <p className='ms-auto d-inline font-light-thick'>{item.email}</p>
+                                                    </div>
+                                                    <div className='d-flex px-3' style={{ fontSize: '13px' }}>
+                                                        <p className='d-inline fw-semibold'>Mobile</p>
+                                                        <p className='ms-auto d-inline font-light-thick'>{item.number}</p>
+                                                    </div>
+                                                    <div className='d-flex justify-content-end mt-3'>
+                                                        <button className='btn btn-sm btn-outline-success me-2 bg-light' onClick={()=>approveDeliveryBoy(item._id)}><i className="bi bi-check-circle" ></i></button>
+                                                        <button className='btn btn-sm btn-outline-danger me-3 bg-light' onClick={()=>deleteDeliveryBoy(item._id)}><i className="bi bi-trash"></i></button>
+                                                    </div>
                                                 </div>
-                                                <p className='ms-3 fw-bold'>{item.name}</p>
-                                                <div className='d-flex px-3 mt-3' style={{ fontSize: '13px' }}>
-                                                    <p className='d-inline fw-semibold'>Email</p>
-                                                    <p className='ms-auto d-inline font-light-thick'>{item.email}</p>
-                                                </div>
-                                                <div className='d-flex px-3' style={{ fontSize: '13px' }}>
-                                                    <p className='d-inline fw-semibold'>Mobile</p>
-                                                    <p className='ms-auto d-inline font-light-thick'>{item.no}</p>
-                                                </div>
-                                                <div className='d-flex justify-content-evenly mt-3'>
-                                                    {state && <i className="bi bi-check-circle bg-light rounded-3 px-2 py-1 box-shadow font-success" onClick={()=>{approveBoy(index)}}></i>}
-                                                    <i className="bi bi-x-circle bg-light rounded-3 px-2 py-1 box-shadow font-danger"></i>
-                                                    <i className="bi bi-trash3 bg-light rounded-3 px-2 py-1 box-shadow font-dark"></i>
-                                                </div>
-                                            </div>
-                                        )
-                                    })
+                                            )
+                                        })
                                     }
+
                                 </div>
                             </div>
                         </div>
