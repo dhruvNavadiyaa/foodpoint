00cc80a4e329bd66db826ada1cfe727f081eac72
dhruvvN <dhruvna9@gmail.com>
2024-03-26 22:00:09 +0530

    qwe

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Footer from './Footer';
 import '../css/Home.css'; // Make sure to create and import the CSS file
 import Navbar from './Navbar';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios'
 
 const Home = () => {
@@ -20,7 +20,7 @@ const Home = () => {
       console.log('Error fetching data:', error);
     }
   }
-  
+
   const getTopProduct = async () => {
     try {
       const response = await axios.get('http://localhost:5000/api/product/top');
@@ -51,7 +51,7 @@ const Home = () => {
               <div className="col d-flex mt-3 scroll-bar py-2" >
 
                 {topProduct.map((item, index) =>
-                  <div key={index}onClick={()=>navigate(`/PlaceOrder/${item?._id}`)}  className="product ms-3 me-md-5 me-3 text-center  ">
+                  <div key={index} onClick={() => navigate(`/PlaceOrder/${item?._id}`)} className="product ms-3 me-md-5 me-3 text-center  ">
                     <img src={item.img || "https://images.pexels.com/photos/1262302/pexels-photo-1262302.jpeg?auto=compress&cs=tinysrgb&w=600"}
                       className='border rounded rounded-circle product-image box-shadow' alt="" />
                     <p className='fs-6 mt-2 text-secondary fw-medium'>{item?.name}</p>
@@ -74,11 +74,12 @@ const Home = () => {
 
                 {topRestaurant.map((item, index) => {
                   return (
-                    <div className='col-md-4 col-sm-6 col-xs-8 col-12  mb-3  m-auto' onClick={() => { navigate(`/Restaurant/${item._id}`) }} key={index}>
+                    <div className='col-md-4 col-sm-6 col-xs-8 col-12 mb-3 m-auto' onClick={() => { navigate(`/Restaurant/${item._id}`) }} key={index}>
                       <div className=' m-0 mx-3'>
                         <div className=" p-0 rounded rounded-4 overflow-hidden w-100" >
                           <img src={item?.img[0] || "https://images.pexels.com/photos/1603901/pexels-photo-1603901.jpeg?auto=compress&cs=tinysrgb&w=600"} alt=""
                             className='img-fluid w-100 rounded rounded-4'
+                            style={{height:'200px',objectFit:"cover"}}
                           />
                         </div>
                         <div className='px-3 mt-2  w-100'>
@@ -104,46 +105,46 @@ const Home = () => {
 
       </div>
       <div className="container-md px-md-5 mt-4" >
-                        <p className='fs-5 mb-0 mt-3 fw-bold'>Popular Cuisines</p>
-                        <div className="row m-0 mb-5 mt-3">
+        <p className='fs-5 mb-0 mt-3 fw-bold'>Popular Cuisines</p>
+        <div className="row m-0 mb-5 mt-3">
 
-                            {/* ITEM CARDS */}
-                            {
-                                topProduct.map((item, index) => {
+          {/* ITEM CARDS */}
+          {
+            topProduct.map((item, index) => {
 
-                                    return <div className="col-sm-6 px-5-md mb-5" key={index}>
-                                        <div className="item p-3 rounded rounded-4 box-shadow">
-                                            <div className='row m-0'>
-                                                <div className="col d-flex align-items-center">
-                                                    <div>
-                                                        <p className='mb-0 fw-bold text-secondary'>By {item.restaurantDetails?.name}</p>
-                                                        <small className='fw-medium text-secondary'><i className="bi bi-star-fill text-secondary"> </i> {item.rating}/5 &#8226; 20-25 min</small>
-                                                    </div>
-                                                    <i className="bi bi-heart-fill fs-4 me-3 ms-auto text-secondary"></i>
-                                                </div>
-                                            </div>
-                                            <hr className='mb-3' />
-                                            <div className="row m-0">
-                                                <div className="col-8 ">
-                                                    <small className='text-warning'><i className="bi bi-star-fill text-warning"> </i>BESTSELLER</small>
-                                                    <p className='fw-bold mb-0'>{item?.name}</p>
-                                                    <small className='fw-medium'>&#x20B9; {item.price}</small><br />
-                                                    <small className='fw-medium text-secondary'>{item.description}.</small>
-                                                </div>
-                                                <div className="col-4 ">
-                                                    <img src={item?.img || "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww"}
-                                                        className='img-fluid rounded rounded box-shadow' alt="" />
-                                                    <button onClick={()=>navigate(`/PlaceOrder/${item._id}`)} className='btn btn-outline-success btn-sm'>BUY NOW</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                }
-                                )
-                            }
-
-                        </div>
+              return <div className="col-sm-6 px-5-md mb-5" key={index}>
+                <div className="item p-3 rounded rounded-4 box-shadow">
+                  <div className='row m-0'>
+                    <div className="col d-flex align-items-center">
+                      <div>
+                        <p className='mb-0 fw-bold text-secondary'>By {item.restaurantDetails?.name}</p>
+                        <small className='fw-medium text-secondary'><i className="bi bi-star-fill text-secondary"> </i> {item.rating}/5 &#8226; 20-25 min</small>
+                      </div>
+                      <i className="bi bi-heart-fill fs-4 me-3 ms-auto text-secondary"></i>
                     </div>
+                  </div>
+                  <hr className='mb-3' />
+                  <div className="row m-0">
+                    <div className="col-8 ">
+                      <small className='text-warning'><i className="bi bi-star-fill text-warning"> </i>BESTSELLER</small>
+                      <p className='fw-bold mb-0'>{item?.name}</p>
+                      <small className='fw-medium'>&#x20B9; {item.price}</small><br />
+                      <small className='fw-medium text-secondary'>{item.description}.</small>
+                    </div>
+                    <div className="col-4 ">
+                      <img src={item?.img || "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww"}
+                        className='img-fluid rounded rounded box-shadow' alt="" />
+                      <button onClick={() => navigate(`/PlaceOrder/${item._id}`)} className='btn btn-outline-success btn-sm'>BUY NOW</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            }
+            )
+          }
+
+        </div>
+      </div>
       <Footer />
     </>
   );
--- frontend/src/component/OrderList.js
+++ frontend/src/component/OrderList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import axios from 'axios'
 import { useSelector } from 'react-redux'
+import '../css/Modal.css'
 
 export default function OrderList() {
 
@@ -9,6 +10,12 @@ export default function OrderList() {
     const user_id = useSelector(state => state.user.userInfo.userInfo._id)
     const [orderPending, setOrderPending] = useState([])
     const [orderDone, setOrderDone] = useState([])
+    const [modalState, setModalState] = useState({
+        isVisible: false,
+        type: null, // Could be 'approve' or 'reject'
+        id: null, // Data you want to pass to the modal
+    });
+    const [rating, setRating] = useState(0);
 
     const getallOrder = async () => {
         const response = await axios.post('http://localhost:5000/api/order/orderinfo', { user_id })
@@ -19,6 +26,7 @@ export default function OrderList() {
             }
             else if (item?._id === "group1") {
                 setOrderDone(item?.orders)
+                console.log(response.data.orderInfo)
             }
         })
 
@@ -27,6 +35,11 @@ export default function OrderList() {
     useEffect(() => {
         getallOrder()
     }, [])
+
+    const handleStarClick = (index) => {
+        setRating(index);
+    };
+
     return (
         <>
             <Navbar />
@@ -86,21 +99,21 @@ export default function OrderList() {
                 </div>
 
                 {/* ORDER HISTORY */}
-                <div className="" style={{ marginTop: '10px' }}>
+                <div className="mt-2">
 
                     <div className="container-md  p-0 px-md-5 px-sm-3 popular mt-2">
                         <p className='fs-5 mb-0 mt-3 fw-bold'>Order History</p>
                         <div className="row m-0">
 
-                            <div className="container-md px-md-3 p-0 mt-4" >
+                            <div className="container-md px-md-3 p-0 mt-5" >
                                 {orderDone.map((item, index) => {
                                     return (<div key={index} className="row m-0 mb-5">
 
-                                        <div className="col px-md-5 mb-5">
-                                            <div className="item p-3 rounded rounded-4 box-shadow">
+                                        <div className="col px-md-5 mb-2">
+                                            <div className="item p-sm-3 p-1 rounded rounded-4 box-shadow">
 
                                                 <div className="row m-0">
-                                                    <div className="col ">
+                                                    <div className="col p-0 p-sm-3">
                                                         <div className='row m-0'>
                                                             <div className="col d-flex align-items-center">
                                                                 <div>
@@ -121,10 +134,10 @@ export default function OrderList() {
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <div className="col-4 ">
+                                                    <div className="col-4 text-center">
                                                         <img src={item?.productDetail.img || "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww"}
-                                                            className='img-fluid rounded rounded box-shadow' alt="" />
-                                                        {/* <button className='btn btn-outline-primary btn-sm'>Give Riview</button> */}
+                                                            className='img-fluid rounded rounded box-shadow w-100' alt="" style={{ height: '200px', objectFit: "cover" }} />
+                                                        <button className='btn btn-outline-primary btn-sm' onClick={() => { setModalState({ isVisible: true, id: item._id }) }}>Give Riview</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -137,14 +150,33 @@ export default function OrderList() {
                     </div>
                 </div>
             </div>
-            {/* <div className="col d-flex align-items-center">
-                                                <div>
-                                                    <p className='mb-0 fw-bold text-secondary'>By McDonald's</p>
-                                                    <small className='fw-medium text-secondary'><i className="bi bi-star-fill text-secondary"> </i> 4/5 &#8226; 20-25 min</small>
-                                                </div>
-                                                <i className="bi bi-heart-fill fs-4 me-3 ms-auto text-secondary"></i>
-                                            </div> */}
-            {/* <hr className='mb-3' /> */}
+
+            {/* MODAL */}
+            <div className="modal-overlay" hidden={!modalState.isVisible}>
+                <div className="modal-content bg-light p-4 box-shadow">
+                    <div className='text-center'>
+                        <img src="https://www.svgrepo.com/show/19684/review.svg" alt="" className='w-25' />
+                        <p className='fs-3 fw-bold mt-2'>Love our food? Rate us!</p>
+                        <div className='d-flex fs-4 text-warning mt-4 justify-content-center'>
+                            {[...Array(5)].map((star, index) => {
+                                index += 1;
+                                return (
+                                    <i
+                                        key={index}
+                                        className={`bi bi-star${index <= rating ? '-fill' : ''} mx-1`}
+                                        style={{ cursor: 'pointer' }}
+                                        onClick={() => handleStarClick(index)}
+                                    ></i>
+                                );
+                            })}
+                        </div>
+                    </div>
+                    <div className="modal-actions d-flex ms-auto mt-auto">
+                        <button className='btn btn-secondary me-2 px-3' onClick={() => setModalState({ isVisible: false, type: null, data: null })}>Cancel</button>
+                        <button className='btn btn-danger px-3'>Submit</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
