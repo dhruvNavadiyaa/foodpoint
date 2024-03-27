ee89963d7f7037330122cced9d2c9b1ae78b6ec1
talaviyayash <95530124+talaviyayash@users.noreply.github.com>
2024-03-27 16:14:16 +0530

    Added Review Api

--- backend/controller/ReviewCtrl.js
+++ backend/controller/ReviewCtrl.js
@@ -7,16 +7,23 @@ const addReview = async( req, res) => {
     const findOrder = await Order.findById(req?.body?.OrderId)
     const findProduct = await Product.findById(findOrder?.products?.product)
     const findRestaurant = await Restaurant.findById(findOrder?.restaurant)
-    console.log(findRestaurant,findOrder,findProduct)
     const updateOrder = await Order.findByIdAndUpdate(req?.body?.OrderId, {
         isreviewGiven:true,
-        // rating : req?.body?.Rating
         rating : req?.body?.Rating
     })
     const reviewtoProduct = ((findProduct.totlaReview * findProduct.rating ) + req?.body?.Rating)/(findProduct.totlaReview +1)
-    console.log(reviewtoProduct)
+    const reviewtoRestaurant = ((findRestaurant.totlaReview * findRestaurant.rating ) + req?.body?.Rating)/(findRestaurant.totlaReview +1)
     const updateProduct = await Product.findByIdAndUpdate(findOrder?.products?.product,{
-        totlaReview
+        totlaReview  : findProduct.totlaReview +1,
+        rating : reviewtoProduct
+    },{new: true})
+
+     const updateRestaurant = await Restaurant.findByIdAndUpdate(findOrder?.restaurant,{
+        totlaReview  : findRestaurant.totlaReview +1,
+        rating : reviewtoRestaurant
+    },{new: true})
+    return res.send({
+        success : true
     })
 }
 
