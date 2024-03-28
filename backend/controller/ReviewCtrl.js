a9fef065eb39ce530d68c3c0d6cc227e9ac551bc
talaviyayash <95530124+talaviyayash@users.noreply.github.com>
2024-03-28 13:25:41 +0530

    Bug Fixing

--- backend/controller/ReviewCtrl.js
+++ backend/controller/ReviewCtrl.js
@@ -1,33 +1,41 @@
-import Order from '../models/OrderModel.js' 
-import Product from '../models/ProductModel.js' 
-import Restaurant from '../models/RestaurantModel.js' 
+import Order from "../models/OrderModel.js";
+import Product from "../models/ProductModel.js";
+import Restaurant from "../models/RestaurantModel.js";
 
+const addReview = async (req, res) => {
+  const findOrder = await Order.findById(req?.body?.OrderId);
+  const findProduct = await Product.findById(findOrder?.products?.product);
+  const findRestaurant = await Restaurant.findById(findOrder?.restaurant);
+  const updateOrder = await Order.findByIdAndUpdate(req?.body?.OrderId, {
+    isreviewGiven: true,
+    rating: req?.body?.Rating,
+  });
+  const reviewtoProduct =
+    (findProduct.totlaReview * findProduct.rating + req?.body?.Rating) /
+    (findProduct.totlaReview + 1);
+  const reviewtoRestaurant =
+    (findRestaurant.totlaReview * findRestaurant.rating + req?.body?.Rating) /
+    (findRestaurant.totlaReview + 1);
+  const updateProduct = await Product.findByIdAndUpdate(
+    findOrder?.products?.product,
+    {
+      totlaReview: findProduct.totlaReview + 1,
+      rating: reviewtoProduct,
+    },
+    { new: true }
+  );
 
-const addReview = async( req, res) => {
-    console.log(req?.body?.Rating)
-    const findOrder = await Order.findById(req?.body?.OrderId)
-    const findProduct = await Product.findById(findOrder?.products?.product)
-    const findRestaurant = await Restaurant.findById(findOrder?.restaurant)
-    const updateOrder = await Order.findByIdAndUpdate(req?.body?.OrderId, {
-        isreviewGiven:true,
-        rating : req?.body?.Rating
-    })
-    const reviewtoProduct = ((findProduct.totlaReview * findProduct.rating ) + req?.body?.Rating)/(findProduct.totlaReview +1)
-    const reviewtoRestaurant = ((findRestaurant.totlaReview * findRestaurant.rating ) + req?.body?.Rating)/(findRestaurant.totlaReview +1)
-    const updateProduct = await Product.findByIdAndUpdate(findOrder?.products?.product,{
-        totlaReview  : findProduct.totlaReview +1,
-        rating : reviewtoProduct
-    },{new: true})
+  const updateRestaurant = await Restaurant.findByIdAndUpdate(
+    findOrder?.restaurant,
+    {
+      totlaReview: findRestaurant.totlaReview + 1,
+      rating: reviewtoRestaurant,
+    },
+    { new: true }
+  );
+  return res.send({
+    success: true,
+  });
+};
 
-     const updateRestaurant = await Restaurant.findByIdAndUpdate(findOrder?.restaurant,{
-        totlaReview  : findRestaurant.totlaReview +1,
-        rating : reviewtoRestaurant
-    },{new: true})
-    return res.send({
-        success : true
-    })
-}
-
-
-
-export {addReview}
+export { addReview };
