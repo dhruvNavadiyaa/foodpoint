a48c48d029a4062f0a679d4bab892df4774f94e9
dhruvvN <dhruvna9@gmail.com>
2024-04-07 22:23:03 +0530

    bug

--- owner/src/components/Home.js
+++ owner/src/components/Home.js
@@ -47,7 +47,7 @@ export default function Home() {
   const userId = useSelector(state => state?.restaurant?.RestaurantInfo?._id)
   useEffect(() => {
     getProduct()
-    console.log(userId)
+    // console.log(userId)
   }, [])
 
   return (
@@ -120,7 +120,9 @@ export default function Home() {
                     </div>
                     <div className="col-4 ">
                       <img src={item?.img ||"https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww"}
-                        className='img-fluid rounded rounded box-shadow' alt="" />
+                        className='img-fluid rounded rounded box-shadow' alt="" 
+                        style={{height:"100px", width:"100%",objectFit:'cover'}}
+                        />
                     </div>
                   </div>
                 </div>
